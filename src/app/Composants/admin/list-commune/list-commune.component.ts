46f27c0e96585ca89723d3307a092493d88346bf
SerigneFallouNiang <fallouniang776@gmail.com>
2024-08-15 23:56:45 +0000

    creation des composants et des services

---
+++ src/app/Composants/admin/list-commune/list-commune.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-commune',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-commune.component.html',
+  styleUrl: './list-commune.component.css'
+})
+export class ListCommuneComponent {
+
+}
