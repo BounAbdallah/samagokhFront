f5dcb5edbe80b143255b22494d3354972893a2ae
SerigneFallouNiang <fallouniang776@gmail.com>
2024-08-16 01:20:23 +0000

    list roles

--- src/app/Composants/admin/roles/roles.component.ts
+++ src/app/Composants/admin/roles/roles.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { RoleService } from '../../../Services/adminServices/role/role.service';
+import { CommonModule } from '@angular/common';
+
+interface PaginatedResponse<T> {
+  current_page: number;
+  data: T[];
+  // ... autres propriétés de pagination
+}
+
+interface Role {
+  id: number;
+  name: string;
+  permissions: any[]; // Ajustez selon la structure exacte de vos permissions
+}
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
+export class RolesComponent implements OnInit{roles: Role[] = [];
+  currentPage: number = 1;
+  totalPages: number = 1;
 
+  constructor(private roleService: RoleService) {}
+
+  ngOnInit(): void {
+    this.loadRoles();
+  }
+
+  loadRoles() {
+    this.roleService.getRoles().subscribe(
+      (response: PaginatedResponse<Role>) => {
+        console.log('Response received:', response);
+        this.roles = response.data;
+        this.currentPage = response.current_page;
+        // Calculez le nombre total de pages si nécessaire
+        // this.totalPages = ...
+        console.log('Roles:', this.roles);
+      },
+      error => console.error('Erreur lors du chargement des rôles', error)
+    );
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RolesComponent } from './Composants/admin/roles/roles.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ { path: 'roles', component: RolesComponent},
+//   { path: 'roles/new', component: RolesComponent },
+//   { path: 'roles/edit/:id', component: RoleFormComponent },
+];
