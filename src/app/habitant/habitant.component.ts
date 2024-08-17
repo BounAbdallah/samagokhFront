f073fe33efeb5360c884118cf6511c3e7cb548c2
NdeyeMbombeCisse <ndeyecisse188@gmail.com>
2024-08-17 13:23:55 +0000

    integration de la page des d'acceuil des habitant

---
+++ src/app/habitant/habitant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-habitant',
+  standalone: true,
+  imports: [],
+  templateUrl: './habitant.component.html',
+  styleUrl: './habitant.component.css'
+})
+export class HabitantComponent {
+
+}
