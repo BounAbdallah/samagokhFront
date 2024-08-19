c174c0e0c445881827da6f099dcf4f02f61afbdf
BounAbdallah <bigmabo61345@gmail.com>
2024-08-19 12:46:19 +0000

    Ajoout de la page liste de habitants

--- src/app/Composants/Maire/page-liste-habitants/page-liste-habitants.component.ts
+++ src/app/Composants/Maire/page-liste-habitants/page-liste-habitants.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SideBarComponent } from "../side-bar/side-bar.component";
+import { ListeHabitantsComponent } from "../liste-habitants/liste-habitants.component";
 
 @Component({
   selector: 'app-page-liste-habitants',
   standalone: true,
-  imports: [],
+  imports: [SideBarComponent, ListeHabitantsComponent],
   templateUrl: './page-liste-habitants.component.html',
   styleUrl: './page-liste-habitants.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
-import { DashboardComponent } from './Composants/Maire/dashboard/dashboard.component';
 import { Routes } from '@angular/router';
 import path from 'path';
+import { DashboardComponent } from './Composants/Maire/dashboard/dashboard.component'
 import { PageDetailsprojetComponent } from './Composants/Maire/page-detailsprojet/page-detailsprojet.component';
+import { PageListeHabitantsComponent } from './Composants/Maire/page-liste-habitants/page-liste-habitants.component';
 
 export const routes: Routes = [
 
 
   {path: "Dashboard", component: DashboardComponent},
   {path: "detail-projet", component: PageDetailsprojetComponent},
+  { path: "Nos-habitants", component: PageListeHabitantsComponent },
 
 ];
