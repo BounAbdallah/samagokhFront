b3a4ef290b849415f8c9db4dc4993f5d2f51813c
SerigneFallouNiang <fallouniang776@gmail.com>
2024-08-19 12:25:28 +0000

    liste-card dashbord

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,28 @@ import { RolesComponent } from './Composants/admin/roles/roles.component';
 import { ListCommuneComponent } from './Composants/admin/list-commune/list-commune.component';
 import { VilleComponent } from './Composants/admin/ville/ville.component';
 import { DashboardComponent } from './Composants/Maire/dashboard/dashboard.component';
+import path from 'path';
+import { BaseDashboardComponent } from './base-dashboard/base-dashboard.component';
+import { DashbordComponent } from './Composants/admin/dashbord/dashbord.component';
 
 export const routes: Routes = [
- { path: 'roles', component: RolesComponent},
- { path: 'dashboard', component: DashboardComponent },
-  //Route par defaut
-  {path:"",pathMatch:'full',redirectTo:'dashboard'},
-  { path: 'communes', component: ListCommuneComponent },
-  { path: 'villes', component:VilleComponent },
+//  { path: 'roles', component: RolesComponent},
+//  { path: 'dashboard', component: DashboardComponent },
+//   //Route par defaut
+//   {path:"",pathMatch:'full',redirectTo:'dashboard'},
+//   { path: 'communes', component: ListCommuneComponent },
+//   { path: 'villes', component:VilleComponent },
+{
+  path: '',
+  component: BaseDashboardComponent,
+  children: [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashbordComponent },
+    { path: 'communes', component: ListCommuneComponent },
+    { path: 'villes', component: VilleComponent },
+    { path: 'roles', component: RolesComponent },
+    // Ajoutez d'autres routes enfants ici
+  ]
+}
  
 ];
