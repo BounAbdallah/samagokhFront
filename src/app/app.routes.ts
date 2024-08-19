f5742136f0534d76bc9999344fc79c50ca4e7196
BounAbdallah <bigmabo61345@gmail.com>
2024-08-19 16:14:37 +0000

    Ajout de la page liste de projets de la mairie

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { DashboardComponent } from './Composants/Maire/dashboard/dashboard.component'
 import { PageDetailsprojetComponent } from './Composants/Maire/page-detailsprojet/page-detailsprojet.component';
 import { PageListeHabitantsComponent } from './Composants/Maire/page-liste-habitants/page-liste-habitants.component';
+import { PageListeProjetMairieComponent } from './Composants/Maire/page-liste-projet-mairie/page-liste-projet-mairie.component';
 
 export const routes: Routes = [
 
@@ -12,5 +13,6 @@ export const routes: Routes = [
   {path: "detail-projet", component: PageDetailsprojetComponent},
   { path: "Nos-habitants", component: PageListeHabitantsComponent },
   { path: "Projet-soumis", component: PageListeProjetSoumisComponent },
+  { path: "Projet-mairie", component: PageListeProjetMairieComponent },
 
 ];
